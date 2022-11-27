5b4890b1ab234293ee5af370cbe3e6a0bf8ce36d
Lil-El <yxd99324@qq.com>
2022-11-27 12:45:54 +0800

    Qt create process.

--- dnd.cpp
+++ dnd.cpp
@@ -3,6 +3,7 @@
 #include <QMimeData>
 #include <QFile>
 #include <QClipboard>
+#include <QProcess>
 
 dnd::dnd(QWidget *parent) : QWidget(parent), ui(new Ui::Dnd)
 {
@@ -81,3 +82,42 @@ void dnd::on_pushButton_2_clicked()
     ui->textEdit->append(str);
 }
 
+// 二进制，无法正常读取
+void dnd::on_pushButton_3_clicked()
+{
+    QFile file("file.txt");
+    file.open(QIODevice::WriteOnly);
+    QDataStream out(&file);
+
+    // Write a header with a "magic number" and a version
+    out << (quint32)0x12345678;
+    out << (qint32)123;
+
+    out.setVersion(QDataStream::Qt_6_4);
+}
+
+
+void dnd::on_pushButton_4_clicked()
+{
+    QFile file("file.bat");
+    file.open(QIODevice::WriteOnly);
+    QTextStream out(&file);
+    out << "echo Mino吖.";
+
+    // stream.setCodec("UTF-8"); 默认使用系统本地编码，也可以使用setCodec设置新的编码
+}
+
+// QProcess::readAll() 函数可以读出程序输出内容。我们使用这个函数将所有的输出获取之后，由于它的返回结果是 QByteArray 类型，所以再转换成 QString 显示出来。
+// 另外注意一点，中文本 Windows 使用的是 GBK 编码，而 Qt 使用的是 Unicode 编码，因此需要做一下转换，否则是会出现乱码的，大家可以尝试一下。
+// QProcess 可以实现进程过程的监听，或者是令Qt 程序等待这个进程结束后再继续执行的函数。
+void dnd::on_pushButton_5_clicked()
+{
+    QProcess *p = new QProcess(this);
+    p->start("cmd.exe", QStringList() << "/c" << "dir"); // 第二个参数是传给ext或bat的参数
+    connect(p, SIGNAL(finished(int)), this, SLOT(readResult(int)));
+}
+
+void dnd::readResult(int exitCode)
+{
+    qDebug() << exitCode;
+}
--- dnd.h
+++ dnd.h
@@ -27,6 +27,14 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void readResult(int exitCode);
+
 private:
     Ui::Dnd *ui;
     bool readFile(const QString &fileName);
